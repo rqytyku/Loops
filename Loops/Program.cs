8b2c685891c461388aa400a2fd751052c62eae6c
Romela Qytyku <rqytyku2@uet.edu.al>
2020-04-07 12:33:56 +0200

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			int data;
+
+
+			Console.Write("\n\n");
+			Console.Write("Ditet e javes se dyte te prillit 6-12 prill  : \n");
+			Console.Write("-------------------------------------------");
+			Console.Write("\n\n");
+
+
+			Console.Write("Jep daten: ");
+			data = Convert.ToInt32(Console.ReadLine());
+				switch (data)
+				{
+					case 6:
+						Console.Write("E Hene\n");
+						break;
+					case 7:
+						Console.Write("E Marte \n");
+						break;
+					case 8:
+						Console.Write("E Merkure \n");
+						break;
+					case 9:
+						Console.Write("E Enjte \n");
+						break;
+					case 10:
+						Console.Write("E Premte \n");
+						break;
+					case 11:
+						Console.Write("E Shtune \n");
+						break;
+					case 12:
+						Console.Write("E Diele \n");
+
+						break;
+
+					default:
+						Console.Write(" Data nuk i perket kesaj jave . \n Provo brenda dates 6-12 prill....\n");
+						break;
+
+				}
+				Console.ReadKey();
+		}
+		
+	}
+	
+}
+
